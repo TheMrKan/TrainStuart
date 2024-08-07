50c589a73e4afd277cf83411ce2c1bec8aced2f1
TheMrKan <mrkan2017@gmail.com>
2024-08-07 20:57:09 +0400

    реализация serial интерфейса

--- robot/hardware/serial_interface.py
+++ robot/hardware/serial_interface.py
@@ -1,9 +1,9 @@
 import serial
 from multiprocessing import Process, Manager, Event
-from typing import Optional
+from typing import Optional, List
 from utils.cancelations import CancellationToken, await_event
 from robot.config import instance as config
-from robot.hardware.serial_process import SharedProtocol, begin
+from robot.hardware.serial_process import SharedProtocol, begin, Command, Request
 
 
 process: Process
@@ -12,6 +12,7 @@ on_state_changed: Event
 on_confirmation_received: Event
 on_completion_received: Event
 on_response_received: Event
+on_message_sent: Event
 manager: Manager
 
 
@@ -27,6 +28,7 @@ def setup():
     global on_confirmation_received
     global on_completion_received
     global on_response_received
+    global on_message_sent
 
     manager = Manager()
     shared = manager.Namespace()
@@ -40,11 +42,12 @@ def setup():
     on_confirmation_received = Event()
     on_completion_received = Event()
     on_response_received = Event()
+    on_message_sent = Event()
     shared.response_code = None
     shared.response_args = None
 
     process = Process(target=begin,
-                      args=(shared, on_state_changed, on_confirmation_received, on_completion_received, on_response_received),
+                      args=(shared, on_state_changed, on_confirmation_received, on_completion_received, on_response_received, on_message_sent),
                       name="robot.hardware.serial_process")
     process.start()
 
@@ -60,17 +63,64 @@ def setup():
         raise InterfaceUnavailableError()
 
 
-def send_command(command_key: str, *args: list, await_confirmation: bool = True, await_completion: bool = False):
-    raise NotImplementedError
+def send_command(command_code: str,
+                 *args: int,
+                 _await_confirmation: bool = True,
+                 _await_completion: bool = False,
+                 completion_timeout: Optional[int] = None,
+                 completion_cancellation: Optional[CancellationToken] = None):
+    command = Command(command_code, *args)
+
+    on_message_sent.clear()
+    on_confirmation_received.clear()
+    on_completion_received.clear()
+
+    shared.outgoing_message = command
+    on_message_sent.wait()
+    on_message_sent.clear()
+
+    if _await_confirmation:
+        await_confirmation()
+
+    if _await_completion:
+        await_completion(completion_timeout, completion_cancellation)
 
 
-def request(request_key: str, *args: list) -> list:
-    raise NotImplementedError
+def send_request(request_code: str,
+                 *args: int,
+                 timeout: Optional[int] = None,
+                 cancellation: Optional[CancellationToken] = None) -> List[int]:
+    request = Request(request_code, *args)
+
+    on_message_sent.clear()
+    on_response_received.clear()
+    shared.response_code = None
+    shared.response_args = None
+
+    shared.outgoing_message = request
+    on_message_sent.wait()
+    on_message_sent.clear()
+
+    return await_response(request_code, timeout, cancellation)
 
 
 def await_confirmation(timeout: Optional[float] = None, cancellation: Optional[CancellationToken] = None):
-    raise NotImplementedError
+    await_event(on_confirmation_received, timeout, cancellation)
+    on_confirmation_received.clear()
+
+
+def await_completion(timeout: Optional[float] = None, cancellation: Optional[CancellationToken] = None):
+    await_event(on_completion_received, timeout, cancellation)
+    on_completion_received.clear()
 
 
 def await_response(code: str, timeout: Optional[float] = None, cancellation: Optional[CancellationToken] = None) -> list:
-    raise NotImplementedError
+    wrong_left = 3
+    while wrong_left:
+        await_event(on_response_received, timeout, cancellation)
+        if shared.response_code != code:
+            wrong_left -= 1
+            continue
+        on_response_received.clear()
+        return shared.response_args or []
+    raise RuntimeError("Received several responses with wrong codes")
--- robot/hardware/serial_process.py
+++ robot/hardware/serial_process.py
@@ -1,7 +1,27 @@
+import time
+
 import serial
 from multiprocessing import Event
-from typing import Protocol, Optional
+from typing import Protocol, Optional, List, Tuple
 import traceback
+
+
+class Command:
+    code: str
+    args: Tuple[int]
+
+    def __init__(self, code: str, *args: int):
+        self.code = code
+        self.args = args
+
+    def __str__(self):
+        str_args = ' '.join(map(str, self.args))
+        return f"{self.code}{' ' + str_args if str_args else ''}\n"
+
+
+class Request(Command):
+    def __str__(self):
+        return "?" + super().__str__()
 
 
 class SharedProtocol(Protocol):
@@ -11,6 +31,7 @@ class SharedProtocol(Protocol):
     exception: Optional[Exception]
     response_code: Optional[str]
     response_args: Optional[list]
+    outgoing_message: Optional[Command]
 
 
 shared: SharedProtocol
@@ -19,25 +40,28 @@ on_state_changed: Event
 on_confirmation_received: Event
 on_completion_received: Event
 on_response_received: Event
+on_message_sent: Event
 
 
-def begin(_shared: SharedProtocol, _on_state_changed, _on_confirmation_received, _on_completion_received, _on_response_received):
+def begin(_shared: SharedProtocol, _on_state_changed, _on_confirmation_received, _on_completion_received, _on_response_received, _on_message_sent):
     global shared
     global connection
     global on_state_changed
     global on_confirmation_received
     global on_completion_received
     global on_response_received
+    global on_message_sent
 
     on_state_changed = _on_state_changed
     on_confirmation_received = _on_confirmation_received
     on_completion_received = _on_completion_received
     on_response_received = _on_response_received
+    on_message_sent = _on_message_sent
 
     shared = _shared
     connection = None
     try:
-        connection = serial.Serial(shared.serial_name, 115200, timeout=5)
+        connection = serial.Serial(shared.serial_name, 115200, timeout=0.1)
         shared.is_connected = True
     except Exception as e:
         shared.exception = e
@@ -52,25 +76,40 @@ def loop():
     global shared
     global connection
 
+    buffer: List[str] = []
+
     while shared.is_running and connection.is_open:
         try:
-            line = read_serial()
+            line = read_serial(buffer)
+            if line:
+                handle_line(line)
 
-            if try_handle_confirmation(line):
-                continue
+            send_outgoing_message()
 
-            if try_handle_completion(line):
-                continue
-
-            if is_response(line):
-                handle_response(line)
+            time.sleep(0.1)
         except:
             traceback.print_exc()
 
 
-def read_serial() -> str:
-    bdata = connection.readline()
-    return bdata.decode().strip()
+def read_serial(buffer: List[str]) -> Optional[str]:
+    while connection.in_waiting:
+        raw = connection.read()
+        char = raw.decode()
+        if char == "\n":
+            line = "".join(buffer)
+            buffer.clear()
+            return line
+        buffer.append(char)
+
+
+def handle_line(line: str):
+    if try_handle_confirmation(line):
+        return
+
+    if try_handle_completion(line):
+        return
+
+    try_handle_response(line)
 
 
 def try_handle_confirmation(line: str) -> bool:
@@ -89,11 +128,10 @@ def try_handle_completion(line: str) -> bool:
     return True
 
 
-def is_response(line: str):
-    return line.startswith("!")
+def try_handle_response(line: str) -> bool:
+    if not line.startswith("!"):
+        return False
 
-
-def handle_response(line: str):
     line = line[1:]    # убираем !
     splitted = line.split(" ")
 
@@ -106,3 +144,13 @@ def handle_response(line: str):
     shared.response_code = code
     shared.response_args = args
     on_response_received.set()
+
+
+def send_outgoing_message():
+    if not shared.outgoing_message:
+        return
+
+    command_str = str(shared.outgoing_message)
+    connection.write(command_str.encode("ascii"))
+    shared.outgoing_message = None
+    on_message_sent.set()
