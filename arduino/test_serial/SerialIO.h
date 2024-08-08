ba92b70c4c18e32a6ceab8d125f31323b4ca1fb5
TheMrKan <mrkan2017@gmail.com>
2024-08-08 18:45:12 +0400

    Arduino: методы для отправки подтверждений выполнения. Рефакторинг

--- arduino/test_serial/SerialIO.h
+++ arduino/test_serial/SerialIO.h
@@ -1,11 +1,21 @@
 #include "Arduino.h"
 
+const byte COMMAND = 0;
+const byte REQUEST = 1;
+const byte RESPONSE = 1;
+const int INT_MIN = -pow(2, 15) + 1;
+
 class SerialIO {
   public:
     SerialIO();
     struct Message readMessage();
     void sendMessage(struct Message message);
-    struct Message produceMessage(byte type, String code);
+    void sendConfirmation();
+    void sendCompletion();
+    int countArgs(int args[]);
+    struct Message produceMessage(byte type, String code, 
+                                  int a0 = INT_MIN, int a1 = INT_MIN, int a2 = INT_MIN, int a3 = INT_MIN, int a4 = INT_MIN,
+                                  int a5 = INT_MIN, int a6 = INT_MIN, int a7 = INT_MIN, int a8 = INT_MIN, int a9 = INT_MIN);
 
   private:
     String buffer = "";
--- arduino/test_serial/test_serial.ino
+++ arduino/test_serial/test_serial.ino
@@ -1,5 +1,7 @@
 #include "SerialIO.h"
 
+#define DEBUG_SERIAL true
+
 SerialIO IO;
 
 struct Message currentMessage;
@@ -12,20 +14,37 @@ void loop() {
   struct Message newMessage = IO.readMessage();
   if (newMessage.code != "") {
     handleMessage(newMessage);
-    struct Message outgoingMessage = IO.produceMessage(1, "Hd");
-    outgoingMessage.args[0] = 120;
-    IO.sendMessage(outgoingMessage);
-    
   }
 }
 
 void handleMessage(struct Message message) {
   currentMessage = message;
+
+  #if DEBUG_SERIAL
+    echoMessageDebug(message);
+  #endif
+
+  if (message.type == COMMAND) {
+    delay(2000);    // симуляция выполнения команды
+    IO.sendCompletion();
+  }
+  else {
+    delay(100);    // симуляция обработки данных с датчика
+    struct Message outgoingMessage = IO.produceMessage(RESPONSE, "Hd", 120);
+    IO.sendMessage(outgoingMessage);
+  }
+  
+}
+
+#if DEBUG_SERIAL
+void echoMessageDebug(struct Message message) {
+  
   Serial.println("Type: " + String(message.type));
   Serial.println("Code: " + message.code);
   Serial.print("Args:");
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < IO.countArgs(message.args); i++) {
     Serial.print(" " + String(message.args[i]));
   }
   Serial.println();
 }
+#endif
