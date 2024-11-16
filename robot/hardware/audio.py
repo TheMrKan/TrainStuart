9cd2da852bad94866bc300a20d690382a9a7d00c
TheMrKan <mrkan2017@gmail.com>
2024-11-16 12:09:31 +0400

    воспроизведение аудио

---
+++ robot/hardware/audio.py
@@ -0,0 +1,56 @@
+import wave
+import sys
+import threading
+import pyaudio
+from utils.cancelations import CancellationToken
+from typing import Optional
+
+
+class AudioOutput:
+
+    CHUNK = 1024
+
+    _playing_thread: Optional[threading.Thread] = None
+    _cancellation: Optional[CancellationToken] = None
+
+    @classmethod
+    def play(cls, filename: str, cancellation: CancellationToken = None):
+        with wave.open(filename, 'rb') as wf:
+            p = pyaudio.PyAudio()
+            try:
+
+                stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                                channels=wf.getnchannels(),
+                                rate=wf.getframerate(),
+                                output=True)
+                try:
+
+                    while len(data := wf.readframes(cls.CHUNK)) and (not cancellation or not cancellation.cancellation_event.is_set()):
+                        stream.write(data)
+                finally:
+                    stream.close()
+            finally:
+                p.terminate()
+
+    @classmethod
+    def interrupt(cls):
+        if cls._playing_thread is not None and cls._playing_thread.is_alive():
+            if cls._cancellation is not None:
+                cls._cancellation.cancel()
+                cls._playing_thread.join()
+
+    @classmethod
+    def _play_and_clear(cls, filename: str):
+        try:
+            cls.play(filename, cls._cancellation)
+        finally:
+            cls._playing_thread = None
+            cls._cancellation = None
+
+    @classmethod
+    def play_async(cls, filename: str):
+        #cls.interrupt()
+
+        cls._cancellation = CancellationToken()
+        cls._playing_thread = threading.Thread(target=cls._play_and_clear, args=(filename, ))
+        cls._playing_thread.start()
